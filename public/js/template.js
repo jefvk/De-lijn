735d3733adeac9b82f2c3f5573dcf6e613ce6b69
jerodepr <Depraetere.Jeroen1@gmail.com>
2017-12-06 13:45:13 +0100

    Code v2
    Hier is de laatste versie van de codes

--- public/js/template.js
+++ public/js/template.js
@@ -14,8 +14,46 @@ var tijd = document.getElementById("tijd");
 var uitklap = document.getElementById("uitklap");
 var bevestigen = document.getElementById("bevestigen");
 var vertrek = document.getElementById("vertrek");
-var tijdstipinvullen = document.getElementById("uur")
-var daginvullen = document.getElementById("dag")
+var tijdstipinvullen = document.getElementById("uur");
+var daginvullen = document.getElementById("dag");
+var knopvandaag = document.getElementById("vandaag");
+var knopmorgen = document.getElementById("morgen");
+var knopkalender = document.getElementById("kalender");
+var knopvertrek = document.getElementById("vertrekknop");
+var knopaankomst = document.getElementById("aankomstknop");
+var kalender = document.getElementById("datum");
+
+knopvertrek.onclick = function(){
+  vertrek.innerHTML = "Vertrek";
+  knopvertrek.className = "link2";
+  knopaankomst.className = "link";
+}
+knopaankomst.onclick = function(){
+  vertrek.innerHTML = "Aankomst";
+  knopvertrek.className = "link";
+  knopaankomst.className = "link2";
+}
+knopvandaag.onclick = function(){
+  daginvullen.innerHTML = "Vandaag";
+  knopvandaag.className = "link2";
+  knopmorgen.className = "link";
+  knopkalender.className = "link";
+  kalender.className = "datum";
+}
+knopmorgen.onclick = function(){
+  daginvullen.innerHTML = "Morgen";
+  knopvandaag.className = "link";
+  knopmorgen.className = "link2";
+  knopkalender.className = "link";
+  kalender.className = "datum";
+}
+knopkalender.onclick = function(){
+  daginvullen.innerHTML = "Kalender";
+  knopvandaag.className = "link";
+  knopmorgen.className = "link";
+  knopkalender.className = "link2";
+  kalender.className = "datum2";
+}
 tijd.onclick = function tonen(){
   uitklap.className = "uitklap2";
   bevestigen.className ="bevestigen2";
@@ -25,35 +63,6 @@ tijd.onclick = function tonen(){
 bevestigen.onclick = function() {
   uitklap.className = "uitklap";
   bevestigen.className ="bevestigen";
-  var vertrekaankomst = document.getElementsByName('vertrekaankomst');
-  var val= "";
-  for (var i = 0, length = vertrekaankomst.length; i < length; i++) {
-      if (vertrekaankomst[i].checked) {
-         val = vertrekaankomst[i].value;
-         break;
-       }
-  }
-
-  if (val == "vertrek" ) {
-    vertrek.innerHTML = "Vertrek";
-  } else {
-    vertrek.innerHTML ="Aankomst";
-  }
-  var tijdstip = document.getElementById("tijdstip").value;
-  tijdstipinvullen.innerHTML = tijdstip;
-
-  var dag = document.getElementsByName('vertrekaankomst');
-  var val= "";
-  for (var i = 0, length = vertrekaankomst.length; i < length; i++) {
-      if (vertrekaankomst[i].checked) {
-         val = vertrekaankomst[i].value;
-         break;
-       }
-  }
-
-  if (val == "vertrek" ) {
-    vertrek.innerHTML = "Vertrek";
-  } else {
-    vertrek.innerHTML ="Aankomst";
-  }
+   var uur = document.getElementById("tijdstip").value;
+   tijdstipinvullen.innerHTML = uur;
 };
