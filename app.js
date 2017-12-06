3d71ba331747ed9975fb16e3f81941731dff46ca
jef vankerckhoven <jef@jefvankerckhoven.be>
2017-12-06 19:51:49 +0100

    werkend

--- app.js
+++ app.js
@@ -5,60 +5,78 @@ var request = require("request");
 var bodyParser = require('body-parser');
 var request = require("request");
 
-var getAddress = "https://maps.googleapis.com/maps/api/geocode/json?address=Wiekevorst&key=AIzaSyDMtUsmbGsJnu_99XdMzB3b5h_XPPMEmf4";
-request.get({url:getAddress, json:true}, (err,res,data)=>{
-  if (err) {
-    console.log(err);
-  }else if (res.statusCode === 200){
-    console.log(data.results[0].geometry.location)
-  }
-})
-
-var getCoor= "https://www.delijn.be/rise-api-core/coordinaten/convert/51.10914/4.79449149999";
-request.get({url:getCoor, json:true}, (err,res,data)=>{
-  if (err) {
-    console.log(err);
-  }else if (res.statusCode === 200){
-    console.log(data.xCoordinaat);
-    console.log(data.yCoordinaat);
-  }
-})
-
-var url = "https://www.delijn.be/rise-api-core/haltes/indebuurt/179815/199980/300";
-request.get({url:url, json:true}, (err,res,data)=>{
-  if (err) {
-    console.log(err);
-  }else if (res.statusCode === 200){
-    console.log(data[0].afstand);
-    console.log(data[1].afstand);
-    console.log(data[2].afstand);
-    console.log(data[3].afstand)
-  }
-})
-
-app.set('nieuwsFile', require('./config/nieuws.json'));
-app.set('categorieenFile', require('./config/categorieen.json'));
-
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
 
-app.post('/', function(request, response){
-    console.log(request.body.tijdstip);
-    console.log(request.body.Van);
-    console.log(request.body.Naar);
-});
+var xVan = "";
+var yVan = "";
+var xNaar = "";
+var yNaar = "";
+
+var duration = "";
+
+app.post('/', function(req, res){
+    var getAddressVan = 'https://maps.googleapis.com/maps/api/geocode/json?address='+ req.body.Van +'&key=AIzaSyDMtUsmbGsJnu_99XdMzB3b5h_XPPMEmf4';
+    request.get({url:getAddressVan, json:true}, (err,res,data)=>{
+      if (err) {
+        console.log(err);
+      }else if (res.statusCode === 200){
+        var getCoorVan= 'https://www.delijn.be/rise-api-core/coordinaten/convert/' +data.results[0].geometry.location.lat+'/' + data.results[0].geometry.location.lng;
+        request.get({url:getCoorVan, json:true}, (err,res,data)=>{
+          if (err) {
+            console.log(err);
+          }else if (res.statusCode === 200){
+             xVan = data.xCoordinaat;
+             yVan = data.yCoordinaat;
+          }
+        })
+      }
+    });
+
+    var getAddressNaar = 'https://maps.googleapis.com/maps/api/geocode/json?address='+ req.body.Naar +'&key=AIzaSyDMtUsmbGsJnu_99XdMzB3b5h_XPPMEmf4';
+    request.get({url:getAddressNaar, json:true}, (err,res,data)=>{
+      if (err) {
+        console.log(err);
+      }else if (res.statusCode === 200){
+        var getCoorNaar= 'https://www.delijn.be/rise-api-core/coordinaten/convert/' +data.results[0].geometry.location.lat+'/' + data.results[0].geometry.location.lng;
+        request.get({url:getCoorNaar, json:true}, (err,res,data)=>{
+          if (err) {
+            console.log(err);
+          }else if (res.statusCode === 200){
+            xNaar = data.xCoordinaat;
+            yNaar = data.yCoordinaat;
+          }
+        })
+      }
+    });
+
+
+    var finalUrl = 'https://www.delijn.be/rise-api-core/reisadvies/routes/'+ req.body.Van + '/' +req.body.Naar +'/'+xVan+'/'+yVan+'/'+xNaar+'/'+yNaar+'/07-12-2017/20:00/1/on/on/on/on/of/nl';
+    request.get({url:finalUrl, json:true}, (err,res,data)=>{
+      if (err) {
+        console.log(err);
+      }else if (res.statusCode === 200){
+          console.log(data);
+
+          duration = data.duration[0];
+          }
+        });
+
+    console.log(duration);
+    });
+
+
+
 
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static('public'))
-
-app.use(require("./routes/root_router"));
-app.use(require("./routes/nieuws_router"));
-app.use(require("./routes/categorieen_router"));
-
+app.get('/', function(req,res){
+  res.render("index", {});
+});
 app.listen(app.get('port'), function() {
   console.log('Node luistert op poort', app.get('port'));
 });
