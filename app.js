67b4afea439cf8bf8af41c10ac6768d7ab3e6a6f
jerodepr <depraetere.jeroen1@gmail.com>
2017-12-06 17:06:34 +0100

    nieuwe ejs

--- app.js
+++ app.js
@@ -56,6 +56,9 @@ app.use(express.static('public'))
 app.use(require("./routes/root_router"));
 app.use(require("./routes/nieuws_router"));
 app.use(require("./routes/categorieen_router"));
+app.get('/resultaten',function(req,res){
+    res.render("resultaten",{});
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node luistert op poort', app.get('port'));
