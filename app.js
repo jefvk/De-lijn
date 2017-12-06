e390dd905bc03f541912400df93b1dbb6b102c04
stijnc97 <stijn1997@live.be>
2017-12-06 13:55:26 +0100

    loggen input

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 var express = require("express");
 var path = require("path");
 var app = express();
-
+var request = require("request");
+var bodyParser = require('body-parser');
+var url = 'https://www.delijn.be/rise-api-core/haltes/indebuurt/103787/194286/300';
+request.get({url:url, json: true}, (err,res,data)=>{
+  if (err) {
+    console.log(err);
+  } else if (res.statusCode === 200){
+    console.log(data[0].bestemmingen);
+  }
+});
 app.set('nieuwsFile', require('./config/nieuws.json'));
 app.set('categorieenFile', require('./config/categorieen.json'));
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.post('/', function(request, response){
+    console.log(request.body.tijdstip);
+    console.log(request.body.Van);
+    console.log(request.body.Naar);
+});
 
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
