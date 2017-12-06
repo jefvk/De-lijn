7f4ce740813d99530fa5ce888b00a6964726754d
stijnc97 <stijn1997@live.be>
2017-12-06 13:59:44 +0100

    Conversie delijn -> google

--- app.js
+++ app.js
@@ -3,14 +3,39 @@ var path = require("path");
 var app = express();
 var request = require("request");
 var bodyParser = require('body-parser');
-var url = 'https://www.delijn.be/rise-api-core/haltes/indebuurt/103787/194286/300';
-request.get({url:url, json: true}, (err,res,data)=>{
+var request = require("request");
+
+var getAddress = "https://maps.googleapis.com/maps/api/geocode/json?address=Wiekevorst&key=AIzaSyDMtUsmbGsJnu_99XdMzB3b5h_XPPMEmf4";
+request.get({url:getAddress, json:true}, (err,res,data)=>{
   if (err) {
     console.log(err);
-  } else if (res.statusCode === 200){
-    console.log(data[0].bestemmingen);
+  }else if (res.statusCode === 200){
+    console.log(data.results[0].geometry.location)
   }
-});
+})
+
+var getCoor= "https://www.delijn.be/rise-api-core/coordinaten/convert/51.10914/4.79449149999";
+request.get({url:getCoor, json:true}, (err,res,data)=>{
+  if (err) {
+    console.log(err);
+  }else if (res.statusCode === 200){
+    console.log(data.xCoordinaat);
+    console.log(data.yCoordinaat);
+  }
+})
+
+var url = "https://www.delijn.be/rise-api-core/haltes/indebuurt/179815/199980/300";
+request.get({url:url, json:true}, (err,res,data)=>{
+  if (err) {
+    console.log(err);
+  }else if (res.statusCode === 200){
+    console.log(data[0].afstand);
+    console.log(data[1].afstand);
+    console.log(data[2].afstand);
+    console.log(data[3].afstand)
+  }
+})
+
 app.set('nieuwsFile', require('./config/nieuws.json'));
 app.set('categorieenFile', require('./config/categorieen.json'));
 
