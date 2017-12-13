5224f3a2cbdae16856ade681ec5f338a37c3f08f
stijnc97 <stijn1997@live.be>
2017-12-13 14:47:28 +0100

    Werkende versie Bijna finaal

--- app.js
+++ app.js
@@ -4,16 +4,30 @@ var app = express();
 var request = require("request");
 var bodyParser = require('body-parser');
 
-
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static('public'))
+app.get('/', function(req,response){
+  response.render("index", {});
+});
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 var xVan = "";
 var yVan = "";
 var xNaar = "";
 var yNaar = "";
 
-var duration = "";
+var startTijd;
+var startHalte;
 
-app.post('/', function(req, res){
+var eindTijd;
+var eindHalte;
+app.post('/', function(req, response){
+
     var getAddressVan = 'https://maps.googleapis.com/maps/api/geocode/json?address='+ req.body.Van +'&key=AIzaSyDMtUsmbGsJnu_99XdMzB3b5h_XPPMEmf4';
     request.get({url:getAddressVan, json:true}, (err,res,data)=>{
       if (err) {
@@ -26,53 +40,59 @@ app.post('/', function(req, res){
           }else if (res.statusCode === 200){
              xVan = data.xCoordinaat;
              yVan = data.yCoordinaat;
-          }
-        })
-      }
+             console.log("coordinaten van oke");
+             console.log(xVan);
+             console.log(yVan);
+
+
+             var getAddressNaar = 'https://maps.googleapis.com/maps/api/geocode/json?address='+ req.body.Naar +'&key=AIzaSyDMtUsmbGsJnu_99XdMzB3b5h_XPPMEmf4';
+             request.get({url:getAddressNaar, json:true}, (err,res,data)=>{
+               if (err) {
+                 console.log(err);
+               }else if (res.statusCode === 200){
+                 var getCoorNaar= 'https://www.delijn.be/rise-api-core/coordinaten/convert/' +data.results[0].geometry.location.lat+'/' + data.results[0].geometry.location.lng;
+                 request.get({url:getCoorNaar, json:true}, (err,res,data)=>{
+                   if (err) {
+                     console.log(err);
+                   }else if (res.statusCode === 200){
+                     xNaar = data.xCoordinaat;
+                     yNaar = data.yCoordinaat;
+                     console.log("coordinaten naar oke");
+                     console.log(xNaar);
+                     console.log(yNaar);
+
+                     console.log(req.body.Van);
+                     console.log(req.body.Naar);
+                     console.log(yVan);
+                     console.log(xVan);
+                     var finalUrl = 'https://www.delijn.be/rise-api-core/reisadvies/routes/'+ req.body.Van + '/' +req.body.Naar +'/'+xVan+'/'+yVan+'/'+xNaar+'/'+yNaar+'/20-12-2017/08:00/1/on/of/of/of/of/nl';
+                     request.get({url:finalUrl, json:true}, (err,res,data)=>{
+                       if (err) {
+                         console.log(err);
+                       }else if (res.statusCode === 200){
+                         console.log(data.reiswegen[0].reiswegStappen[1]);
+                         startTijd = data.reiswegen[0].reiswegStappen[1].start;
+                         startHalte = data.reiswegen[0].reiswegStappen[1].startLocatie;
+                         response.render("resultaten", {
+                           "startTijd": startTijd,
+                           "startHalte": startHalte
+                         });
+                           }
+                         });
+                   }
+                 })
+               }
+          })
+        }
+      })
+    };
+    });
     });
 
-    var getAddressNaar = 'https://maps.googleapis.com/maps/api/geocode/json?address='+ req.body.Naar +'&key=AIzaSyDMtUsmbGsJnu_99XdMzB3b5h_XPPMEmf4';
-    request.get({url:getAddressNaar, json:true}, (err,res,data)=>{
-      if (err) {
-        console.log(err);
-      }else if (res.statusCode === 200){
-        var getCoorNaar= 'https://www.delijn.be/rise-api-core/coordinaten/convert/' +data.results[0].geometry.location.lat+'/' + data.results[0].geometry.location.lng;
-        request.get({url:getCoorNaar, json:true}, (err,res,data)=>{
-          if (err) {
-            console.log(err);
-          }else if (res.statusCode === 200){
-            xNaar = data.xCoordinaat;
-            yNaar = data.yCoordinaat;
-          }
-        })
-      }
-    });
-
-
-    var finalUrl = 'https://www.delijn.be/rise-api-core/reisadvies/routes/'+ req.body.Van + '/' +req.body.Naar +'/'+xVan+'/'+yVan+'/'+xNaar+'/'+yNaar+'/07-12-2017/20:00/1/on/on/on/on/of/nl';
-    request.get({url:finalUrl, json:true}, (err,res,data)=>{
-      if (err) {
-        console.log(err);
-      }else if (res.statusCode === 200){
-          console.log(data);
-
-          duration = data.reiswegen[0].duration;
-          }
-        });
-
-    console.log(duration);
-    });
 
 
 
 
-app.set("views", path.resolve(__dirname, "views"));
-app.set("view engine", "ejs");
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static('public'))
-app.get('/', function(req,res){
-  res.render("index", {});
-});
 app.listen(app.get('port'), function() {
   console.log('Node luistert op poort', app.get('port'));
 });
--- routes/root_router.js
+++ routes/root_router.js
@@ -3,8 +3,6 @@ var root = express.Router();
 
 root.get('/', function(req, res) {
   res.render("index", {
-    nieuws: req.app.get('nieuwsFile').nieuws,
-    categorieen: req.app.get('categorieenFile').categorieen
   });
 });
 
