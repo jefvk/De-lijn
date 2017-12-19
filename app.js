05e3eacbfffd6e277b86234b27e5bd670295abdc
jef vankerckhoven <jef.vankerckhoven@student.ap.be>
2017-12-19 22:57:14 +0100

    FINAL CODE HET WERKT JIPPIEE!!!

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var app = express();
 var request = require("request");
 var bodyParser = require('body-parser');
 
+
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 app.set('port', (process.env.PORT || 5000));
@@ -50,6 +51,10 @@ var startHalte5;
 var lijnNummer5;
 var eindTijd5;
 var eindHalte5;
+
+
+
+
 app.post('/', function(req, response){
 
     var getAddressVan = 'https://maps.googleapis.com/maps/api/geocode/json?address='+ req.body.Van +'&key=AIzaSyDMtUsmbGsJnu_99XdMzB3b5h_XPPMEmf4';
@@ -64,10 +69,6 @@ app.post('/', function(req, response){
           }else if (res.statusCode === 200){
              xVan = data.xCoordinaat;
              yVan = data.yCoordinaat;
-             console.log("coordinaten van oke");
-             console.log(xVan);
-             console.log(yVan);
-
 
              var getAddressNaar = 'https://maps.googleapis.com/maps/api/geocode/json?address='+ req.body.Naar +'&key=AIzaSyDMtUsmbGsJnu_99XdMzB3b5h_XPPMEmf4';
              request.get({url:getAddressNaar, json:true}, (err,res,data)=>{
@@ -81,83 +82,55 @@ app.post('/', function(req, response){
                    }else if (res.statusCode === 200){
                      xNaar = data.xCoordinaat;
                      yNaar = data.yCoordinaat;
-                     console.log("coordinaten naar oke");
-                     console.log(xNaar);
-                     console.log(yNaar);
 
-                     console.log(req.body.Van);
-                     console.log(req.body.Naar);
-                     console.log(yVan);
-                     console.log(xVan);
-                     var finalUrl = 'https://www.delijn.be/rise-api-core/reisadvies/routes/'+ req.body.Van + '/' +req.body.Naar +'/'+xVan+'/'+yVan+'/'+xNaar+'/'+yNaar+'/'+datum+'/'+tijd+'/1/on/of/of/of/of/nl';
+                     var finalUrl = 'https://www.delijn.be/rise-api-core/reisadvies/routes/'+ req.body.Van + '/' +req.body.Naar +'/'+xVan+'/'+yVan+'/'+xNaar+'/'+yNaar+'/'+req.body.Dag+'-'+req.body.Maand+'-'+req.body.Jaar+'/'+ req.body.Tijdstip+'/'+ req.body.vertrek+'/on/of/of/of/of/nl';
+                     console.log(finalUrl);
                      request.get({url:finalUrl, json:true}, (err,res,data)=>{
                        if (err) {
                          console.log(err);
                        }else if (res.statusCode === 200){
-                         startTijd = data.reiswegen[0].reiswegStappen[1].start;
-                         startHalte = data.reiswegen[0].reiswegStappen[1].startLocatie;
-                         lijnNummer = data.reiswegen[0].reiswegStappen[1].lijn.lijnNummerPubliek;
-                         eindTijd = data.reiswegen[0].reiswegStappen[1].end;
-                         eindHalte = data.reiswegen[0].reiswegStappen[1].aankomstLocatie;
+                          if (data.reiswegen[0] === undefined) {
+                             response.render("404",{});
+                             console.log(data.reiswegen[0]);
+                          } else {
+                            startTijd = data.reiswegen[0].reiswegStappen[1].start;
+                            startHalte = data.reiswegen[0].reiswegStappen[1].startLocatie;
+                            lijnNummer = data.reiswegen[0].reiswegStappen[1].lijn.lijnNummerPubliek;
+                            eindTijd = data.reiswegen[0].reiswegStappen[1].end;
+                            eindHalte = data.reiswegen[0].reiswegStappen[1].aankomstLocatie;
 
-                         startTijd2 = data.reiswegen[1].reiswegStappen[1].start;
-                         startHalte2 = data.reiswegen[1].reiswegStappen[1].startLocatie;
-                         lijnNummer2 = data.reiswegen[1].reiswegStappen[1].lijn.lijnNummerPubliek;
-                         eindTijd2 = data.reiswegen[1].reiswegStappen[1].end;
-                         eindHalte2 = data.reiswegen[1].reiswegStappen[1].aankomstLocatie;
+                            startTijd2 = data.reiswegen[1].reiswegStappen[1].start;
+                            startHalte2 = data.reiswegen[1].reiswegStappen[1].startLocatie;
+                            lijnNummer2 = data.reiswegen[1].reiswegStappen[1].lijn.lijnNummerPubliek;
+                            eindTijd2 = data.reiswegen[1].reiswegStappen[1].end;
+                            eindHalte2 = data.reiswegen[1].reiswegStappen[1].aankomstLocatie;
 
-                         startTijd3 = data.reiswegen[2].reiswegStappen[1].start;
-                         startHalte3 = data.reiswegen[2].reiswegStappen[1].startLocatie;
-                         lijnNummer3 = data.reiswegen[2].reiswegStappen[1].lijn.lijnNummerPubliek;
-                         eindTijd3 = data.reiswegen[2].reiswegStappen[1].end;
-                         eindHalte3 = data.reiswegen[2].reiswegStappen[1].aankomstLocatie;
+                            startTijd3 = data.reiswegen[2].reiswegStappen[1].start;
+                            startHalte3 = data.reiswegen[2].reiswegStappen[1].startLocatie;
+                            lijnNummer3 = data.reiswegen[2].reiswegStappen[1].lijn.lijnNummerPubliek;
+                            eindTijd3 = data.reiswegen[2].reiswegStappen[1].end;
+                            eindHalte3 = data.reiswegen[2].reiswegStappen[1].aankomstLocatie;
+                            response.render("resultaten", {
+                              "startTijd": startTijd,
+                              "startHalte": startHalte,
+                              "lijnNummer": lijnNummer,
+                              "eindTijd": eindTijd,
+                              "eindHalte": eindHalte,
 
-                      /*   startTijd4 = data.reiswegen[3].reiswegStappen[1].start;
-                         startHalte4 = data.reiswegen[3].reiswegStappen[1].startLocatie;
-                         lijnNummer4 = data.reiswegen[3].reiswegStappen[1].lijn.lijnNummerPubliek;
-                         eindTijd4 = data.reiswegen[3].reiswegStappen[1].end;
-                         eindHalte4 = data.reiswegen[3].reiswegStappen[1].aankomstLocatie;
+                              "startTijd2": startTijd2,
+                              "startHalte2": startHalte2,
+                              "lijnNummer2": lijnNummer2,
+                              "eindTijd2": eindTijd2,
+                              "eindHalte2": eindHalte2,
 
-                         startTijd5 = data.reiswegen[4].reiswegStappen[1].start;
-                         startHalte5 = data.reiswegen[4].reiswegStappen[1].startLocatie;
-                         lijnNummer5 = data.reiswegen[4].reiswegStappen[1].lijn.lijnNummerPubliek;
-                         eindTijd5 = data.reiswegen[4].reiswegStappen[1].end;
-                         eindHalte5 = data.reiswegen[4].reiswegStappen[1].aankomstLocatie;*/
+                             "startTijd3": startTijd3,
+                              "startHalte3": startHalte3,
+                              "lijnNummer3": lijnNummer3,
+                              "eindTijd3": eindTijd3,
+                              "eindHalte3": eindHalte3
 
-                         response.render("resultaten", {
-                           "startTijd": startTijd,
-                           "startHalte": startHalte,
-                           "lijnNummer": lijnNummer,
-                           "eindTijd": eindTijd,
-                           "eindHalte": eindHalte,
-
-                           "startTijd2": startTijd2,
-                           "startHalte2": startHalte2,
-                           "lijnNummer2": lijnNummer2,
-                           "eindTijd2": eindTijd2,
-                           "eindHalte2": eindHalte2,
-
-                          "startTijd3": startTijd3,
-                           "startHalte3": startHalte3,
-                           "lijnNummer3": lijnNummer3,
-                           "eindTijd3": eindTijd3,
-                           "eindHalte3": eindHalte3,
-
-                          /* "startTijd4": startTijd4,
-                           "startHalte4": startHalte4,
-                           "lijnNummer4": lijnNummer4,
-                           "eindTijd4": eindTijd4,
-                           "eindHalte4": eindHalte4,
-
-                           "startTijd5": startTijd5,
-                           "startHalte5": startHalte5,
-                           "lijnNummer5": lijnNummer5,
-                           "eindTijd5": eindTijd5,
-                           "eindHalte5": eindHalte5,*/
-
-
-
-                         });
+                            });
+                          }
                            }
                          });
                    }
--- public/js/template.js
+++ public/js/template.js
@@ -1,3 +1,17 @@
+window.onload = function() {
+      setTimeout(intro, 4000);
+       main();
+}
+
+function intro(){
+  var vis = document.getElementsByClassName("vis")[0];
+  console.log(vis);
+  var invis = document.getElementsByClassName("invis")[0];
+
+  vis.classList.add('invis');
+  invis.classList.remove("invis");
+}
+
 function main() {
           var figures = document.getElementsByTagName("figure");
           var teller = 0;
@@ -7,64 +21,25 @@ function main() {
           }
 }
 
-window.onload = function() {
-       main();
-}
+
 var tijd = document.getElementById("tijd");
 var uitklap = document.getElementById("uitklap");
 var bevestigen = document.getElementById("bevestigen");
 var vertrek = document.getElementById("vertrek");
 var tijdstipinvullen = document.getElementById("uur");
-var daginvullen = document.getElementById("dag");
-var knopvandaag = document.getElementById("vandaag");
-var knopmorgen = document.getElementById("morgen");
-var knopkalender = document.getElementById("kalender");
-var knopvertrek = document.getElementById("vertrekknop");
-var knopaankomst = document.getElementById("aankomstknop");
-var kalender = document.getElementById("datum");
-var overlaydatum = document.getElementById('overlaydatum');
 var overlaytijd = document.getElementById('overlaytijd');
 var tijdinput = document.getElementById('tijdstip');
-knopvertrek.onclick = function(){
-  vertrek.innerHTML = "Vertrek";
-  knopvertrek.className = "link2";
-  knopaankomst.className = "link";
-}
-knopaankomst.onclick = function(){
-  vertrek.innerHTML = "Aankomst";
-  knopvertrek.className = "link";
-  knopaankomst.className = "link2";
-}
-knopvandaag.onclick = function(){
-  daginvullen.innerHTML = "Vandaag";
-  knopvandaag.className = "link2";
-  knopmorgen.className = "link";
-  knopkalender.className = "link";
-  kalender.className = "datum";
-}
-knopmorgen.onclick = function(){
-  daginvullen.innerHTML = "Morgen";
-  knopvandaag.className = "link";
-  knopmorgen.className = "link2";
-  knopkalender.className = "link";
-  kalender.className = "datum";
-}
-knopkalender.onclick = function(){
-  daginvullen.innerHTML = "Kalender";
-  knopvandaag.className = "link";
-  knopmorgen.className = "link";
-  knopkalender.className = "link2";
-  kalender.className = "datum2";
-}
+var select = document.getElementById("select");
+var wanneer = document.getElementById("dag");
+
+var dag = document.getElementById("daginvuller");
+var maand = document.getElementById("maandinvuller");
+var jaar = document.getElementById("jaarinvuller");
+
+
 tijd.onclick = function tonen(){
   uitklap.className = "uitklap2";
   bevestigen.className ="bevestigen2";
-}
-kalender.onclick = function(){
-overlaydatum.className = "overlaydatum2";
-}
-overlaydatum.onclick = function(){
-overlaydatum.className = "overlaydatum";
 }
 
 tijdinput.onclick = function(){
@@ -74,12 +49,19 @@ overlaytijd.className = "overlaytijd2";
 overlaytijd.onclick = function(){
 overlaytijd.className = "overlaytijd";
 }
-bevestigen.onclick = function() {
+
+
+bevestigen.onclick = function(){
+  if (select.value === "1") {
+    vertrek.innerHTML = "Vertrek";
+  }else {
+    vertrek.innerHTML = "Aankomst"
+  }
+
+  wanneer.innerHTML = dag.value + "/" + maand.value + "/" + jaar.value
+
   uitklap.className = "uitklap";
   bevestigen.className ="bevestigen";
    var uur = document.getElementById("tijdstip").value;
    tijdstipinvullen.innerHTML = uur;
-};
-
-
-
+}
